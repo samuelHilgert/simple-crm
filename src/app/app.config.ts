7dbbcb2cb102c9115825e297fcdd6c87c64eb51d
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-15 15:46:46 +0100

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-4c5f7","appId":"1:412888545149:web:390e290c542a8735c8be14","storageBucket":"simple-crm-4c5f7.firebasestorage.app","apiKey":"AIzaSyBSy4KkaeHQX4Co8q_7SprKCBVCyyKNPqw","authDomain":"simple-crm-4c5f7.firebaseapp.com","messagingSenderId":"412888545149"}))), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase()))]
 };
