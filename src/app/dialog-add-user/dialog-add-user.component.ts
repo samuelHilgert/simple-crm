df5757ee27140c4953964a3353691044439c789b
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-15 16:01:28 +0100

    update

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import {
+  MatFormField,
+  MatFormFieldModule,
+  MatLabel,
+} from '@angular/material/form-field';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { User } from '../models/user.class';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog-add-user',
-  standalone: true,
-  imports: [],
+  imports: [
+    MatDialogContent,
+    MatDialogActions,
+    MatButton,
+    MatDialogTitle,
+    MatInputModule,
+    MatFormFieldModule,
+    MatFormField,
+    MatLabel,
+    MatDatepickerModule,
+    MatButtonModule,
+    FormsModule
+  ],
+  providers: [provideNativeDateAdapter()],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './dialog-add-user.component.html',
-  styleUrl: './dialog-add-user.component.scss'
+  styleUrl: './dialog-add-user.component.scss',
+  standalone: true,
 })
-export class DialogAddUserComponent {
 
+export class DialogAddUserComponent {
+  user = new User();
+  birthDate: Date = new Date();
+
+  saveUser() {
+    this.user.birthDate = this.birthDate ? this.birthDate.getTime() : 0;
+    console.log('current user is: ', this.user);
+  }
 }
---
+++ src/app/models/user.class.ts
@@ -0,0 +1,20 @@
+export class User {
+    firstName: string = '';
+    lastName: string = '';
+    birthDate: number = 0;
+    street: string = '';
+    houseNumber: number = 0;
+    zipCode: number = 0;
+    city: string = '';
+  
+    constructor(obj?: any) {
+      this.firstName = obj ? obj.firstName : '';
+      this.lastName = obj ? obj.lastName : '';
+      this.birthDate = obj ? obj.birthDate : '';
+      this.street = obj ? obj.street : '';
+      this.houseNumber = obj ? obj.houseNumber : '';
+      this.zipCode = obj ? obj.zipCode : '';
+      this.city = obj ? obj.city : '';
+    }
+  }
+  
